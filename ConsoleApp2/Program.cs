3485ab4a00d40f4a418a095cf66757040a90865a
Avit Sidis <avit.sidis@gmail.com>
2017-08-30 13:56:20 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Please replace the connection string attribute settings
+                string constr = "user id=viper;password=VIPER_ts;data source=SampleDataSource";
+
+                OracleConnection con = new OracleConnection(constr);
+                con.Open();
+                Console.WriteLine("Connected to Oracle Database {0}", con.ServerVersion);
+                con.Dispose();
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex);
+            }
+            Console.WriteLine("Press RETURN to exit.");
+            Console.ReadLine();
+        }
+    }
+}
